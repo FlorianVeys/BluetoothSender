644f68e560c892f8fcfbcb388491ad2914f38385
florianveys <florianveys@gmail.com>
2018-09-03 18:55:14 +0200

    Start mainView

--- BluetoothSender-Windows/BluetoothSender-Windows/View/MainWindow.xaml.cs
+++ BluetoothSender-Windows/BluetoothSender-Windows/View/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace BluetoothSender_Windows
+namespace BluetoothSender_Windows.View
 {
     /// <summary>
     /// Logique d'interaction pour MainWindow.xaml
