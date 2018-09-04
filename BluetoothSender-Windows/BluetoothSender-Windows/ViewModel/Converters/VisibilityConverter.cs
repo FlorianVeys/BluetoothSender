1193aad6bc6464d151fbb1b12074e0bee0aeb151
florianveys <florianveys@gmail.com>
2018-09-04 21:49:29 +0200

    MainWindow improvement

---
+++ BluetoothSender-Windows/BluetoothSender-Windows/ViewModel/Converters/VisibilityConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace BluetoothSender_Windows.ViewModel.Converters
+{
+    public class VisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Boolean val = value != null && (Boolean)value;
+            return val ? "Visible" : "Collapsed";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            String val = (String)value;
+            return val != null && val.Equals("Visible");
+
+        }
+    }
+}
--- BluetoothSender-Windows/BluetoothSender-Windows/ViewModel/MainViewModel.cs
+++ BluetoothSender-Windows/BluetoothSender-Windows/ViewModel/MainViewModel.cs
@@ -1,36 +1,76 @@
+using System;
+using System.ComponentModel;
+using System.Windows;
 using System.Windows.Input;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.CommandWpf;
 
 namespace BluetoothSender_Windows.ViewModel
 {
-    /// <summary>
-    /// This class contains properties that the main View can data bind to.
-    /// <para>
-    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
-    /// </para>
-    /// <para>
-    /// You can also use Blend to data bind with the tool's support.
-    /// </para>
-    /// <para>
-    /// See http://www.galasoft.ch/mvvm
-    /// </para>
-    /// </summary>
     public class MainViewModel : ViewModelBase
     {
-        /// <summary>
-        /// Initializes a new instance of the MainViewModel class.
-        /// </summary>
-        
         public ICommand CloseApplication { get; }
+        public ICommand MinimizeApplication { get; }
+        public ICommand MenuControl { get; }
+
+        public bool VisibilityHistory { get; set; }
+        public bool VisibilityEmitter { get; set; }
+        public bool VisibilityReceiver { get; set; }
+
         public MainViewModel()
         {
             CloseApplication = new RelayCommand(CloseAppli);
+            MinimizeApplication = new RelayCommand(MinimizeAppli);
+            MenuControl = new RelayCommand<string>(changeView);
+            DisplayEmitterView();
+        }
+
+        private void changeView(string parameter)
+        {
+            switch (parameter)
+            {
+                case "History":
+                    DisplayHistoryView();
+                    break;
+                case "Emitter":
+                    DisplayEmitterView();
+                    break;
+                case "Receiver":
+                    DisplayReceiverView();
+                    break;
+            }
+        }
+
+        private void DisplayHistoryView()
+        {
+            VisibilityEmitter = false;
+            VisibilityReceiver = false;
+            VisibilityHistory = true;
+        }
+
+        private void DisplayEmitterView()
+        {
+            VisibilityEmitter = true;
+            VisibilityReceiver = false;
+            VisibilityHistory = false;
+        }
+
+        private void DisplayReceiverView()
+        {
+            VisibilityEmitter = false;
+            VisibilityReceiver = true;
+            VisibilityHistory = false;
+
         }
 
         private void CloseAppli()
         {
             App.Current.Shutdown();
+        }
+
+        private void MinimizeAppli()
+        {
+            App.Current.MainWindow.WindowState = WindowState.Minimized;
         }
     }
 }
